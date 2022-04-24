d4903e43ea947573246dc48e1b4ec7b41860c52f
Federico Bagnalasta <federico.bagnalasta@gmail.com>
2022-04-24 15:55:53 +0200

    Second commit

---
+++ Tamagotchi/src/it/unibs/fp/tamagotchi/Main.java
@@ -0,0 +1,10 @@
+package it.unibs.fp.tamagotchi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Tamagotchi/src/it/unibs/fp/tamagotchi/Tamagotchi.java
@@ -0,0 +1,5 @@
+package it.unibs.fp.tamagotchi;
+
+public class Tamagotchi {
+
+}
