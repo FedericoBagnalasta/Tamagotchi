15f770b05a7a40b44daeb3fcf95e2128b5c20d77
Federico Bagnalasta <federico.bagnalasta@gmail.com>
2022-04-30 13:15:02 +0200

    Aggiunta codice da TamaBase

--- Tamagotchi/src/it/unibs/fp/tamagotchi/Main.java
+++ Tamagotchi/src/it/unibs/fp/tamagotchi/Main.java
@@ -1,9 +1,52 @@
 package it.unibs.fp.tamagotchi;
+import it.unibs.fp.mylib.InputDati;
+import it.unibs.fp.mylib.MyMenu;
 
 public class Main {
 
+	private static final String DAI_CAREZZE = "Dai carezze";
+	private static final String DAI_BISCOTTI = "Dai biscotti";
+	private static final String NUM_BISCOTTI = "Inserire il numero di biscotti";
+	private static final String NUM_CAREZZE = "Inserire numero carezze";
+	private static final String RICHIESTA_SODDISFAZIONE = "Inserisci il valore iniziale di soddisfazione (da 0 a 100): ";
+	private static final String RICHIESTA_SAZIETA = "inserisci il valore iniziale di sazieta' (da 0  a 100) : ";
+	private static final String RICHIESTA_NOME = "come si chiama il tuo Tamagotchi?";
+	private static final int MAX_SAZIETA = 100;
+	private static final String Scelta = "Scegli l'opzione";
+	private static final String SALUTO = "Benvenuto nel mondo del Tamagotchi!";
+
 	public static void main(String[] args) {
+		System.out.println(SALUTO);
 		
+		String[] voci = {DAI_BISCOTTI, DAI_CAREZZE};
+		int scelta=0, numBiscotti, numCarezze;
+		Tamagotchi tamagotchi = creaTamagotchi();
+		MyMenu menu=new MyMenu(Scelta, voci);
+		do {
+			scelta=menu.scegli();
+			switch(scelta) {
+				case 1:
+					numBiscotti=InputDati.leggiIntero(NUM_BISCOTTI, 0, 20);
+					tamagotchi.daiBiscotti(numBiscotti);
+					break;
+				case 2:
+					numCarezze=InputDati.leggiIntero(NUM_CAREZZE, 0, 20);
+					tamagotchi.daiCarezze(numCarezze);
+					break;
+				default:
+					break;
+			}
+			System.out.println(tamagotchi.toString());
+		}while(scelta!=0);
+	}
+
+	private static Tamagotchi creaTamagotchi() {
+		String nome=InputDati.leggiStringaNonVuota(RICHIESTA_NOME);
+		int sazieta=InputDati.leggiIntero(RICHIESTA_SAZIETA, 0, MAX_SAZIETA);
+		int soddisfazione=InputDati.leggiIntero(RICHIESTA_SODDISFAZIONE, 0, 100);
+		
+		Tamagotchi tamagotchi=new Tamagotchi(nome, sazieta, soddisfazione);
+		return tamagotchi;
 	}
 
 }
--- Tamagotchi/src/it/unibs/fp/tamagotchi/Tamagotchi.java
+++ Tamagotchi/src/it/unibs/fp/tamagotchi/Tamagotchi.java
@@ -1,5 +1,56 @@
 package it.unibs.fp.tamagotchi;
 
 public class Tamagotchi {
+	public final String FELICE = "\nSono felice";
+	public final String TRISTE = "\nSono triste!";
+	private static final String SODDISFAZIONE = "\nSoddisfazione:";
+	private static final String SAZIETA = "\nSazietà";
+	private static final String NOME = "Nome:";
+	private String nome;
+	private int sazieta;
+	private int soddisfazione;
+	
+	public Tamagotchi(String nome, int sazieta, int soddisfazione) {
+		this.nome = nome;
+		this.sazieta = sazieta;
+		this.soddisfazione = soddisfazione;
+	}
+	
+	 public void daiBiscotti(int numBiscotti){
+		 sazieta=Math.min(100, sazieta+numBiscotti);
+		 soddisfazione=Math.max(0, soddisfazione-5);
+	 }
+
+	public void daiCarezze(int numCarezze) {
+		soddisfazione=Math.min(100, soddisfazione+numCarezze);
+		sazieta=Math.max(0, sazieta-5);
+	}
+	
+	public int getSazieta() {
+		return sazieta;
+	}
+
+	public int getSoddisfazione () {
+		return soddisfazione;
+	}
+	
+	public boolean isTriste() {
+		return(sazieta<30 || soddisfazione<30);
+	}
+	
+	public boolean isFelice() {
+		return(sazieta>80 || soddisfazione>80);
+	}
+	
+	public String toString() {
+		StringBuffer descrizione =new StringBuffer ();
+			descrizione.append(NOME+nome);
+			descrizione.append(SAZIETA+sazieta);
+			descrizione.append(SODDISFAZIONE+soddisfazione);
+			if(isTriste()) descrizione.append(TRISTE);
+			if(isFelice() && !isTriste()) descrizione.append(FELICE);
+			return descrizione.toString();
+			
+	}
 
 }
