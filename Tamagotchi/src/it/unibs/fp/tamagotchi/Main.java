bed4b2330b38cda0550ccbb2ea33f490f23f81eb
matteokovacic <matteokovacic@air-di-matteo>
2022-05-04 11:06:40 +0200

    mancano l'ultima slide del pdf e i test

--- Tamagotchi/src/it/unibs/fp/tamagotchi/Main.java
+++ Tamagotchi/src/it/unibs/fp/tamagotchi/Main.java
@@ -4,16 +4,24 @@ import it.unibs.fp.mylib.MyMenu;
 
 public class Main {
 
+	private static final int MAX_SODDISFAZIONE_INIZIALE= 99;
+	private static final int MIN_SODDISFAZIONE_INIZIALE= 1;
+	private static final int MAX_SAZIETA_INIZIALE= 99;
+	private static final int MIN_SAZIETA_INIZIALE= 1;
+	private static final int MAX_CAREZZE = 20;
+	private static final int MIN_CAREZZE = 0;
+	private static final int MAX_BISCOTTI = 20;
+	private static final int MIN_BISCOTTI = 0;
 	private static final String DAI_CAREZZE = "Dai carezze";
 	private static final String DAI_BISCOTTI = "Dai biscotti";
 	private static final String NUM_BISCOTTI = "Inserire il numero di biscotti";
-	private static final String NUM_CAREZZE = "Inserire numero carezze";
-	private static final String RICHIESTA_SODDISFAZIONE = "Inserisci il valore iniziale di soddisfazione (da 0 a 100): ";
-	private static final String RICHIESTA_SAZIETA = "inserisci il valore iniziale di sazieta' (da 0  a 100) : ";
-	private static final String RICHIESTA_NOME = "come si chiama il tuo Tamagotchi?";
-	private static final int MAX_SAZIETA = 100;
-	private static final String Scelta = "Scegli l'opzione";
+	private static final String NUM_CAREZZE = "Inserire il numero di carezze";
+	private static final String RICHIESTA_SODDISFAZIONE = "Inserisci il valore iniziale di soddisfazione (da 1 a 99): ";
+	private static final String RICHIESTA_SAZIETA = "Inserisci il valore iniziale di sazieta' (da 1 a 99): ";
+	private static final String RICHIESTA_NOME = "Come si chiama il tuo Tamagotchi?";
+	private static final String SCELTA = "Scegli l'opzione";
 	private static final String SALUTO = "Benvenuto nel mondo del Tamagotchi!";
+	
 
 	public static void main(String[] args) {
 		System.out.println(SALUTO);
@@ -21,31 +29,29 @@ public class Main {
 		String[] voci = {DAI_BISCOTTI, DAI_CAREZZE};
 		int scelta=0, numBiscotti, numCarezze;
 		Tamagotchi tamagotchi = creaTamagotchi();
-		MyMenu menu=new MyMenu(Scelta, voci);
+		MyMenu menu=new MyMenu(SCELTA, voci);
 		do {
 			scelta=menu.scegli();
 			switch(scelta) {
 				case 1:
-					numBiscotti=InputDati.leggiIntero(NUM_BISCOTTI, 0, 20);
+					numBiscotti=InputDati.leggiIntero(NUM_BISCOTTI, MIN_BISCOTTI, MAX_BISCOTTI);
 					tamagotchi.daiBiscotti(numBiscotti);
 					break;
 				case 2:
-					numCarezze=InputDati.leggiIntero(NUM_CAREZZE, 0, 20);
+					numCarezze=InputDati.leggiIntero(NUM_CAREZZE, MIN_CAREZZE, MAX_CAREZZE);
 					tamagotchi.daiCarezze(numCarezze);
 					break;
 				default:
 					break;
 			}
 			System.out.println(tamagotchi.toString());
-		}while(scelta!=0);
+		}while(scelta!=0 || !tamagotchi.sonoMorto());
 	}
-	
-	//bagna puzza
 
 	private static Tamagotchi creaTamagotchi() {
 		String nome=InputDati.leggiStringaNonVuota(RICHIESTA_NOME);
-		int sazieta=InputDati.leggiIntero(RICHIESTA_SAZIETA, 0, MAX_SAZIETA);
-		int soddisfazione=InputDati.leggiIntero(RICHIESTA_SODDISFAZIONE, 0, 100);
+		int sazieta=InputDati.leggiIntero(RICHIESTA_SAZIETA, MIN_SAZIETA_INIZIALE, MAX_SAZIETA_INIZIALE);
+		int soddisfazione=InputDati.leggiIntero(RICHIESTA_SODDISFAZIONE, MIN_SODDISFAZIONE_INIZIALE, MAX_SODDISFAZIONE_INIZIALE);
 		
 		Tamagotchi tamagotchi=new Tamagotchi(nome, sazieta, soddisfazione);
 		return tamagotchi;
--- Tamagotchi/src/it/unibs/fp/tamagotchi/Tamagotchi.java
+++ Tamagotchi/src/it/unibs/fp/tamagotchi/Tamagotchi.java
@@ -1,14 +1,30 @@
 package it.unibs.fp.tamagotchi;
 
 public class Tamagotchi {
+	private static final double FATTORE_AUMENTO_SAZIETA = 0.1;
+	private static final int FATTORE_DECREMENTO_SODDISFAZIONE = 4;
+	private static final int FATTORE_DECREMENTO_SAZIETA = 2;
+	private static final int FELICITA_SODDISFAZIONE = 80;
+	private static final int FELICITA_SAZIETA = 80;
+	private static final int TRISTEZZA_MAX_SAZIETA = 80;
+	private static final int TRISTEZZA_MIN_SAZIETA = 30;
+	private static final int TRISTEZZA_MIN_SODDISFAZIONE = 30;
+	private static final int MORTE_MIN_SODDISFAZIONE = 0;
+	private static final int MORTE_MIN_SAZIETA = 0;
+	private static final int MORTE_MAX_SAZIETA = 100;
+	private static final int MIN_SAZIETA = 0;
+	private static final int MAX_SODDISFAZIONE = 100;
+	private static final int MIN_SODDISFAZIONE = 0;
+	private static final int MAX_SAZIETA = 100;
 	public final String FELICE = "\nSono felice";
-	public final String TRISTE = "\nSono triste!";
+	public final String TRISTE = "\nSono triste";
+	public final String MORTO = "\nSono morto";
 	private static final String SODDISFAZIONE = "\nSoddisfazione:";
-	private static final String SAZIETA = "\nSazietà";
+	private static final String SAZIETA = "\nSazietà:";
 	private static final String NOME = "Nome:";
 	private String nome;
-	private int sazieta;
-	private int soddisfazione;
+	private double sazieta;
+	private double soddisfazione;
 	
 	public Tamagotchi(String nome, int sazieta, int soddisfazione) {
 		this.nome = nome;
@@ -16,39 +32,46 @@ public class Tamagotchi {
 		this.soddisfazione = soddisfazione;
 	}
 	
-	 public void daiBiscotti(int numBiscotti){
-		 sazieta=Math.min(100, sazieta+numBiscotti);
-		 soddisfazione=Math.max(0, soddisfazione-5);
-	 }
+	public void daiBiscotti(int numBiscotti){
+		for(int i = 0; i < numBiscotti; i++) {
+			sazieta=Math.min(MAX_SAZIETA, sazieta+sazieta*FATTORE_AUMENTO_SAZIETA);
+		}
+		soddisfazione=Math.max(MIN_SODDISFAZIONE, soddisfazione-numBiscotti/FATTORE_DECREMENTO_SODDISFAZIONE);
+	}
 
 	public void daiCarezze(int numCarezze) {
-		soddisfazione=Math.min(100, soddisfazione+numCarezze);
-		sazieta=Math.max(0, sazieta-5);
+		soddisfazione=Math.min(MAX_SODDISFAZIONE, soddisfazione+numCarezze);
+		sazieta=Math.max(MIN_SAZIETA, sazieta-numCarezze/FATTORE_DECREMENTO_SAZIETA);
 	}
 	
-	public int getSazieta() {
+	public boolean sonoMorto() {
+		return sazieta == MORTE_MAX_SAZIETA || sazieta == MORTE_MIN_SAZIETA || soddisfazione == MORTE_MIN_SODDISFAZIONE;
+	}
+	
+	public double getSazieta() {
 		return sazieta;
 	}
 
-	public int getSoddisfazione () {
+	public double getSoddisfazione () {
 		return soddisfazione;
 	}
 	
-	public boolean isTriste() {
-		return(sazieta<30 || soddisfazione<30);
+	public boolean sonoTriste() {
+		return(soddisfazione<TRISTEZZA_MIN_SODDISFAZIONE || sazieta<TRISTEZZA_MIN_SAZIETA || sazieta>TRISTEZZA_MAX_SAZIETA);
 	}
 	
-	public boolean isFelice() {
-		return(sazieta>80 || soddisfazione>80);
+	public boolean sonoFelice() {
+		return(sazieta>FELICITA_SAZIETA || soddisfazione>FELICITA_SODDISFAZIONE);
 	}
 	
 	public String toString() {
-		StringBuffer descrizione =new StringBuffer ();
+		StringBuffer descrizione=new StringBuffer();
 			descrizione.append(NOME+nome);
 			descrizione.append(SAZIETA+sazieta);
 			descrizione.append(SODDISFAZIONE+soddisfazione);
-			if(isTriste()) descrizione.append(TRISTE);
-			if(isFelice() && !isTriste()) descrizione.append(FELICE);
+			if(sonoTriste()) descrizione.append(TRISTE);
+			if(sonoFelice() && !sonoTriste()) descrizione.append(FELICE);
+			if(sonoMorto()) descrizione.append(MORTO);
 			return descrizione.toString();
 			
 	}
