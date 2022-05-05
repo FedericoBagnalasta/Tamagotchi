a2029f13c6616fa40b96475e86338d3046d62129
Federico Bagnalasta <federico.bagnalasta@gmail.com>
2022-05-05 17:46:45 +0200

    Second commit

--- Tamagotchi/src/it/unibs/fp/tamagotchi/Main.java
+++ Tamagotchi/src/it/unibs/fp/tamagotchi/Main.java
@@ -8,14 +8,8 @@ public class Main {
 	private static final int MIN_SODDISFAZIONE_INIZIALE= 1;
 	private static final int MAX_SAZIETA_INIZIALE= 99;
 	private static final int MIN_SAZIETA_INIZIALE= 1;
-	private static final int MAX_CAREZZE = 20;
-	private static final int MIN_CAREZZE = 0;
-	private static final int MAX_BISCOTTI = 20;
-	private static final int MIN_BISCOTTI = 0;
 	private static final String DAI_CAREZZE = "Dai carezze";
 	private static final String DAI_BISCOTTI = "Dai biscotti";
-	private static final String NUM_BISCOTTI = "Inserire il numero di biscotti";
-	private static final String NUM_CAREZZE = "Inserire il numero di carezze";
 	private static final String RICHIESTA_SODDISFAZIONE = "Inserisci il valore iniziale di soddisfazione (da 1 a 99): ";
 	private static final String RICHIESTA_SAZIETA = "Inserisci il valore iniziale di sazieta' (da 1 a 99): ";
 	private static final String RICHIESTA_NOME = "Come si chiama il tuo Tamagotchi?";
@@ -27,27 +21,29 @@ public class Main {
 		System.out.println(SALUTO);
 		
 		String[] voci = {DAI_BISCOTTI, DAI_CAREZZE};
-		int scelta=0, numBiscotti, numCarezze;
+		int scelta=0;
 		Tamagotchi tamagotchi = creaTamagotchi();
 		MyMenu menu=new MyMenu(SCELTA, voci);
 		do {
 			scelta=menu.scegli();
 			switch(scelta) {
 				case 1:
-					numBiscotti=InputDati.leggiIntero(NUM_BISCOTTI, MIN_BISCOTTI, MAX_BISCOTTI);
-					tamagotchi.daiBiscotti(numBiscotti);
+					tamagotchi.riceviBiscotti();
 					break;
 				case 2:
-					numCarezze=InputDati.leggiIntero(NUM_CAREZZE, MIN_CAREZZE, MAX_CAREZZE);
-					tamagotchi.daiCarezze(numCarezze);
+					tamagotchi.riceviCarezze();
 					break;
 				default:
 					break;
 			}
 			System.out.println(tamagotchi.toString());
-		}while(scelta!=0 || !tamagotchi.sonoMorto());
+		}while(scelta!=0 && !tamagotchi.sonoMorto());
 	}
 
+	/**
+	 * Metodo per istanziare e assegnare valori iniziali al Tamagotchi
+	 * @return Tamagotchi
+	 */
 	private static Tamagotchi creaTamagotchi() {
 		String nome=InputDati.leggiStringaNonVuota(RICHIESTA_NOME);
 		int sazieta=InputDati.leggiIntero(RICHIESTA_SAZIETA, MIN_SAZIETA_INIZIALE, MAX_SAZIETA_INIZIALE);
